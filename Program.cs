2413f0831fb65cc56778c3a6e1f4da7639ea25c8
Stiven Maldonado <smaldonado1@uniminuto.edu.co>
2025-07-11 08:39:25 -0500

    edit front

--- Program.cs
+++ Program.cs
@@ -6,8 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddOcelot();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 await app.UseOcelot();
 
 app.Run();
